8d3e4bcf64662cff55932dd2004504ba4babbbb0
Etec Profº Horácio Augusto da Silveira <joaogiovanneetec@gmail.com>
2023-03-21 08:24:05 -0300

    segundo commit

--- src/Livro.java
+++ src/Livro.java
@@ -3,4 +3,14 @@ public class Livro {
     String resumo;
     double preco;
     int paginas;
+    String autor;
+
+    void exibirDados(){ /* função em Java*/
+        System.out.println("Nome: " + nome);
+        System.out.println("Resumo: " + resumo);
+        System.out.println("Preco: R$" + preco);
+        System.out.println("Paginas: " + paginas);
+        System.out.println("Autor: " + autor);
+        System.out.println("--------");
+    }
 }
--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -7,12 +7,29 @@ public class LivroTeste {
         meuLivro.preco = 50.99;
         meuLivro.paginas = 521;
         meuLivro.resumo = "...";
+        meuLivro.autor = "J. R. Tolkien";
 
-        System.out.println(meuLivro.nome);
-        System.out.println(meuLivro.resumo);
-        System.out.println(meuLivro.preco);
-        System.out.println(meuLivro.paginas);
+        meuLivro.exibirDados();
+
+        System.out.println("\nMeu Livro:\n");
+        System.out.println("Nome: " + meuLivro.nome);
+        System.out.println("Resumo: " + meuLivro.resumo);
+        System.out.println("Preco: R$" + meuLivro.preco);
+        System.out.println("Paginas: " + meuLivro.paginas);
+        System.out.println("Autor: " + meuLivro.autor);
 
         Livro livroFavorito = new Livro();
+        livroFavorito.nome = "Diário de um banana";
+        livroFavorito.resumo = "...";
+        livroFavorito.paginas = 200;
+        livroFavorito.preco = 59.99;
+        livroFavorito.autor = "Jeff Kinney";
+
+        System.out.println("\nLivro Favorito:\n" );
+        System.out.println("Nome: " + livroFavorito.nome);
+        System.out.println("Resumo: " + livroFavorito.resumo);
+        System.out.println("Preco: R$" + livroFavorito.preco);
+        System.out.println("Paginas: " + livroFavorito.paginas);
+        System.out.println("Autor: " + livroFavorito.autor);
     }
 }
